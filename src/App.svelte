f48b32ec0b74abb8be27a5b4bc95ed6777157b46
josephhunta <107034576+josephhunta@users.noreply.github.com>
2023-03-04 12:09:24 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  
+</script>
+
+
+<div class="boxgrid">
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+  <button class="box"></button>
+</div>
+
+
+<style lang="scss" scoped>
+  .boxgrid{
+    height: 500px;
+    width: 500px;
+    padding: 5px;
+    border: 3px solid black;
+    background-color: #FFFCF0;
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    gap: 5px;
+    .box{
+      border: 1px solid black;
+      background-color: #DEB887;
+      &:active{
+        background-color: #be9e74;
+      }
+    }
+  }
+</style>
